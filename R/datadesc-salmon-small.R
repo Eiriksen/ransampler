b66243bed723363730f8a1fb8dc88e04b9b30e8c
Åsheim <asheimer@ad.helsinki.fi>
2023-05-30 11:30:24 +0300

    updated documentation, changed parameter names, and added extra dataset

---
+++ R/datadesc-salmon-small.R
@@ -0,0 +1,10 @@
+#' salmon study population
+#'
+#' example data for ransampler
+#' individual fish data, with length, weight, sex, vgll3 genotype, population, and family information
+#' Smaller dataset with fewer individuals
+#'
+#' @docType data
+#'
+#' @usage data(table_salmon_small)
+"table_salmon_small"
--- R/script - ransampler 2.R
+++ R/script - ransampler 2.R
@@ -3,63 +3,75 @@
 #' Takes a dataframe of individuals (rows) with certain attributes (columns), and samples a given number of individuals from all possible combinations of a given set of paramters
 #' For example, can sample one individual of ech combination of sex and age
 #' Also allows for some control, like, "no individuals sharing the same treatment and family"
+#' Some lingo:
+#' \itemize{
+#' \item type: a specific combination of attributes, e.g "color:red, size:large", as define  
+#' \item combination table: A table that tells the sampler what you are looking for. In the table, each attribute has one column, and each row gives a different combination of attributes. The table is generated automatically based on your dataset and the "ofeach" parameter, but you can also supply it manualy.
+#' }
 #'
 #' @param dataframe The dataframe to get the individuals from. Each row must be a single individual. Columns are attributes.
-#' @param ofEach The attributes we want to randomise over, as a vector of column names, e.g: ofEach=c("sex","age","tank")
-#' @param N_ofEach How many individuals within each combination of the "ofEach" parameters that should be selected
-#' @param noShareWithin One or more sets of paremeters, where unique combinations can't be shared. For example, if we want no more than a single individual from any given family, within a tank, a set would be c("family","tank"), meaning that no individuals can have the same of both family and tank. Supplied within a list, so: list(c("family","tank")). Add more sets within the list if needed, for example: list(c("tank","father"),c("tank","mother)))
-#' @param priBy If some individuals are to be prioritized over others, specify the name of the column containing prioritization info. This must ba number, and lower numbers are prioritized. (E.g, individuals with "1" are prioritized over individuals iwth "2")
-#' @param useDuplis If all individuals that are selected within each combination may be be used, or just one of them.
+#' @param ofeach The attributes we want to randomise over, as a vector of column names, e.g: ofeach=c("sex","age","tank"). Can also be a combination table (data frame).
+#' @param n_ofeach How many individuals within each combination (type) of the "ofeach" parameters that should be selected
+#' @param no_share One or more sets of paremeters, where unique combinations can't be shared. For example, if we want no more than a single individual from any given family, within a tank, a set would be c("family","tank"), meaning that no individuals can have the same of both family and tank. Supplied within a list, so: list(c("family","tank")). Add more sets within the list if needed, for example: list(c("tank","father"),c("tank","mother)))
+#' @param pri_by If some individuals are to be prioritized over others, specify the name of the column containing prioritization info. This must ba number, and lower numbers are prioritized. (E.g, individuals with "1" are prioritized over individuals iwth "2")
+#' @param use_dupli If all individuals that are selected within each combination may be be used, or just one of them.
+#' @param identifier Optional. A name that will be added to the "ID_type" column. 
+#' @param return_combtable if T: Returns the combination table instead of searching for individuals.
+#' @param runs If T: Runs the search multiple times, and returns the run with the highest number of successfully selected individuals
+#' @param reshuffle_combtable If T: When using multiple runs, will reshuffle the order of the combination table, can help with finding individuals of problematic types (combinations)
 #' @export
-ransampler = function(table,ofEach,except,nOfEach=1,noShareWithin=c(),priBy,useDuplis=F,identifier="",returnCombTable=F,runs=1,reshuffle_combinationTable=F){
+ransampler = function(table,ofeach,except,n_of_each=1,no_share=c(),pri_by,use_dupli=F,identifier="",return_combtable=F,runs=1,reshuffle_combtable=F){
   require(glue)
   require(tidyverse)
   require(magrittr)
 
   # Argument handling ---------------------------------------
 
-  # ofEach can be:
+  # ofeach can be:
   #   A vector: Is translated to a combination table
   #   A data.frame: is assumed to be a combination table
   #   A list of data frames: Is merged into a single data.frame
   #   Empty,
-  if(is.vector(ofEach)){
-    table_combinations  <- combinationTable(table, ofEach)
+  if(is.vector(ofeach)){
+    table_combinations  <- combinationTable(table, ofeach)
   }
-  else if (is.list(ofEach)){
-    table_combinations <- bind_rows(ofEach)
+  else if (is.list(ofeach)){
+    table_combinations <- bind_rows(ofeach)
   }
 
-  if (!missing(nOfEach))
+  if (!missing(n_of_each))
   {
-    table_combinations$nOfThis = nOfEach
+    table_combinations$nOfThis = n_of_each
   }
 
   # if exceptionsa are used:
   if (!missing(except)){
     for (exception in except){
-      table_combinations <- table_combinations %>% anti_join(listFilter(df=table_combinations,l=exception,eq=T),by=ofEach)
+      table_combinations <- table_combinations %>% anti_join(listFilter(df=table_combinations,l=exception,eq=T),by=ofeach)
     }
   }
 
-  if(missing(priBy)){
+  if(missing(pri_by)){
     table$internalPri = 1
-    priBy="internalPri"
+    pri_by="internalPri"
   }
 
-  if (returnCombTable==T){
+  if (return_combtable==T){
     return(table_combinations %>%
     combinations_getOptions(table_main=table) %>%
     #arrange(n_options) %>%
     as_tibble())
   }
-  else print(
+  else {
+    message("Searching using the following table of combinations:")
+    print(
     table_combinations %>%
     combinations_getOptions(table_main=table) %>%
     #arrange(n_options) %>%
     as_tibble(),
-  n=500)
-
+    n=500)
+  }
+  
   table_combinations_ori <- table_combinations
   table_selected_best <- NULL
 
@@ -69,7 +81,7 @@ ransampler = function(table,ofEach,except,nOfEach=1,noShareWithin=c(),priBy,useD
 
     if(runs > 1) message(glue("Run {run_current} of {runs}"))
 
-    if (run_current !=1 & reshuffle_combinationTable==T) table_combinations <- shuffle_rows(table_combinations_ori)
+    if (run_current !=1 & reshuffle_combtable==T) table_combinations <- shuffle_rows(table_combinations_ori)
     else table_combinations <- table_combinations_ori
 
 
@@ -108,12 +120,12 @@ ransampler = function(table,ofEach,except,nOfEach=1,noShareWithin=c(),priBy,useD
           table_curCombination_narrow = table_curCombination
 
           # Prioritizing
-          table_curCombination_narrow <- table_curCombination_narrow %>% filter(!!rlang::sym(priBy) == count_pri)
+          table_curCombination_narrow <- table_curCombination_narrow %>% filter(!!rlang::sym(pri_by) == count_pri)
 
           # Checking if this priority is empty
           if (nrow(table_curCombination_narrow) == 0)
           {
-            if (count_pri == max(table_main[[priBy]],na.rm=T))
+            if (count_pri == max(table_main[[pri_by]],na.rm=T))
             {
               is_found <- T
               curCombination["ID_type"] <-  ID_type_cur
@@ -125,12 +137,12 @@ ransampler = function(table,ofEach,except,nOfEach=1,noShareWithin=c(),priBy,useD
             next()
           }
 
-          # Optimizing when useduplis==F
+          # Optimizing when use_dupli==F
           # The following tries its best to have individuals of the same combination use the same noshare combinations
           # At some point, this is not going to work and this step is skipped.
-          if (useDuplis == F & count_layer != 1 & count_level != count_layer)
+          if (use_dupli == F & count_layer != 1 & count_level != count_layer)
           {
-            previous_individual <<- table_selected %>% filter(ID_type == ID_type_cur) %>% filter(level==count_level)
+            previous_individual <- table_selected %>% filter(ID_type == ID_type_cur) %>% filter(level==count_level)
 
             if (previous_individual %>% nrow() == 0)
             {
@@ -138,9 +150,9 @@ ransampler = function(table,ofEach,except,nOfEach=1,noShareWithin=c(),priBy,useD
               next()
             }
 
-            for (noShareCombination in noShareWithin)
+            for (noShareCombination in no_share)
             {
-              parameters_prevIndiv <- previous_individual %>% select(noShareCombination)
+              parameters_prevIndiv <- previous_individual %>% select(all_of(noShareCombination))
               table_curCombination_narrow <- table_curCombination_narrow %>% listFilter(parameters_prevIndiv)
             }
             if (nrow(table_curCombination_narrow) == 0)
@@ -159,10 +171,10 @@ ransampler = function(table,ofEach,except,nOfEach=1,noShareWithin=c(),priBy,useD
            external_conflict <- F
            internal_conflict <- F
            # go through each noshare combination
-           for (noShareCombination in noShareWithin)
+           for (noShareCombination in no_share)
            {
               table_notThese <- table_selected %>% listFilter(curCombination)
-              cur_noShareParameters <- curIndividual %>% select(unlist(noShareCombination)) %>% as.list()
+              cur_noShareParameters <- curIndividual %>% select(all_of(unlist(noShareCombination))) %>% as.list()
               table_conflictIndividuals_ext <- table_selected %>% listFilter(cur_noShareParameters) %>% filter(!ID_num %in% table_notThese$ID_num)
               table_conflictIndividuals_int <- table_selected %>% listFilter(cur_noShareParameters) %>% listFilter(curCombination)
 
@@ -180,7 +192,7 @@ ransampler = function(table,ofEach,except,nOfEach=1,noShareWithin=c(),priBy,useD
            {
             next()
            }
-           if (useDuplis & internal_conflict)
+           if (use_dupli & internal_conflict)
            {
             next()
            }
@@ -201,10 +213,10 @@ ransampler = function(table,ofEach,except,nOfEach=1,noShareWithin=c(),priBy,useD
   if(runs > 1) message(glue("Missing: {sum(is.na(table_selected$level))}"))
 
 
-  if (run_current != 1 & useDuplis==T) {
+  if (run_current != 1 & use_dupli==T) {
     if(sum(is.na(table_selected$level)) < sum(is.na(table_selected_best$level))) table_selected_best <- table_selected
   }
-  else if(run_current != 1 & useDuplis==F){
+  else if(run_current != 1 & use_dupli==F){
     table_selected_layer1 <- table_selected[which(table_selected$layer == 1),]
     table_selected_best_layer1 <- table_selected_best[which(table_selected_best$layer == 1),]
     if(sum(is.na(table_selected_layer1$level)) < sum(is.na(table_selected_best_layer1$level))) table_selected_best <- table_selected
@@ -214,12 +226,12 @@ ransampler = function(table,ofEach,except,nOfEach=1,noShareWithin=c(),priBy,useD
     }
 
   }
-  return(table_selected_best)
+  return(table_selected_best %>% select(-level) %>% rename(n_of_type = layer))
 }
 
 #' @export
 combinationTable = function(dataframe, columns){
-  dataframe %>% select(columns) %>% na.omit() %>% as.list() %>% do.call(crossing,.)
+  dataframe %>% select(all_of(columns)) %>% na.omit() %>% as.list() %>% do.call(crossing,.)
 }
 
 # SKIPS NA values
@@ -248,7 +260,7 @@ combinations_getOptions = function(table_combinations, table_main){
   options = table_combinations %>% apply(MARGIN=1,FUN=function(x){
 
     thisCombination = x %>% as.list()
-    pip <<- thisCombination
+    pip <- thisCombination
     thisIndividuals = table_main %>% listFilter(thisCombination)
     result          = thisIndividuals %>% nrow()
     result
