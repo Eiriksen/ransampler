2ec98b06acf5edcb2f7cbb9375b096e07a8b5d93
Åsheim <asheimer@ad.helsinki.fi>
2023-05-31 12:34:30 +0300

    changed messaging behavoiur for table of combinations

--- R/script - ransampler 2.R
+++ R/script - ransampler 2.R
@@ -20,7 +20,7 @@
 #' @param runs If T: Runs the search multiple times, and returns the run with the highest number of successfully selected individuals
 #' @param reshuffle_combtable If T: When using multiple runs, will reshuffle the order of the combination table, can help with finding individuals of problematic types (combinations)
 #' @export
-ransampler = function(table,ofeach,except,n_ofeach=1,no_share=c(),pri_by,use_dupli=T,identifier="",return_combtable=F,runs=1,reshuffle_combtable=F){
+ransampler = function(table,ofeach,except,n_ofeach=1,no_share=c(),pri_by,use_dupli=T,identifier="",return_combtable=F,runs=1,reshuffle_combtable=T){
   require(glue)
   require(tidyverse)
   require(magrittr)
@@ -62,12 +62,12 @@ ransampler = function(table,ofeach,except,n_ofeach=1,no_share=c(),pri_by,use_dup
   }
   else {
     message("Searching using the following table of combinations:")
-    print(
+    message(
     table_combinations %>%
     combinations_getOptions(table_main=table) %>%
-    #arrange(n_options) %>%
-    as_tibble(),
-    n=500)
+    capture.output() %>%
+    paste0(collapse="\n")
+    )
   }
   
   table_combinations_ori <- table_combinations
